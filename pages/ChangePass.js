446fffaf2c00b4d2cd8bb128a403282ed950ac25
Anurag dhurwey <anuragdhurwey9211@gmail.com>
2023-03-31 14:52:48 +0530

    first commit

---
+++ pages/ChangePass.js
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import { ChangePassword } from './Components/ChangePassword'
+
+ const ChangePass = () => {
+  return (
+    <div>
+<ChangePassword/>
+    </div>
+  )
+}
+
+export default ChangePass
+
